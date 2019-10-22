5c9c7fc30ac3c0ed2c394239f765a011a724680b
Jose Beltran <jbeltran@doterra.com>
2019-10-22 11:48:48 -0600

    login page

--- web/account/index.php
+++ web/account/index.php
@@ -7,17 +7,18 @@ session_start();
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
-   $action = filter_input(INPUT_GET, 'action');
+    $action = filter_input(INPUT_GET, 'action');
 }
 
 
 switch ($action) {
-   case 'login':     
-      header('Location: /view/account.php');
-      break;
+    case 'login':
+        include '/view/account.php';
+        //   header('Location: /view/account.php');
+        break;
 
-   default:
-      include 'home.php';
+    default:
+        include 'home.php';
 
 }
 

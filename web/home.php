9b6f05659e2eb0b16f1a77c28040cf0cda98c35b
Jose Beltran <jbeltran@doterra.com>
2019-10-22 11:33:47 -0600

    login page

--- web/home.php
+++ web/home.php
@@ -24,7 +24,7 @@
 
                 <!-- Icon -->
                 <div class="fadeIn first">
-                    <h2 class="login-title">Login</h2>
+                    <h2 class="login-title">nota app</h2>
                 </div>
 
                 <!-- Login Form -->

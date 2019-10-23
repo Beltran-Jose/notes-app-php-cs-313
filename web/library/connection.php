5a15411eed72b360409d21060ebd0daaeae62254
Jose Beltran <jbeltran@doterra.com>
2019-10-23 12:15:51 -0600

    login page

--- web/library/connection.php
+++ web/library/connection.php
@@ -1,29 +1,22 @@
 <?php
-
-function notesConnect() {
-    try {
-        $dbUrl = getenv('postgres://lqxwheqfmouasz:abb3d2f0c728b83fe256d4ce9901102b85e403cf4009d22bb026230dce526510@ec2-23-21-109-177.compute-1.amazonaws.com:5432/d9vuclponvi0cc');
-        // Get the various parts of the DB Connection from the URL
-        $dbopts = parse_url($dbUrl);
-        $dbHost = $dbopts["host"];
-        $dbPort = $dbopts["port"];
-        $dbUser = $dbopts["user"];
-        $dbPassword = $dbopts["pass"];
-        $dbName = ltrim($dbopts["path"], '/');
-        // Create the PDO connection
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-        // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-        $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $ex) {
-        // If this were in production, you would not want to echo
-        // the details of the exception.
-        echo 'Error!: '.$ex -> getMessage();
-        die();
-    }
-}
-
-notesConnect();
-
-
-
+    /*Function for connecting to the database. Rather than needing every page to connect*/
+    function connect_db() {
+        $db == NULL;
+        try {
+            $dbUrl = getenv('DATABASE_URL');
+            $dbOpts = parse_url($dbUrl);
+            $dbHost = $dbOpts["host"];
+            $dbPort = $dbOpts["port"];
+            $dbUser = $dbOpts["user"];
+            $dbPassword = $dbOpts["pass"];
+            $dbName = ltrim($dbOpts["path"], '/');
+            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+            //echo $dbHost.$dbPort.$dbUser.$dbName;
+            $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+            echo 'Error!: '.$ex -> getMessage();
+            die();
+        }
+        return $db;
+    } 
 ?>

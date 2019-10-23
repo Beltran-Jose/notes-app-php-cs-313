b72a2ed40c158563db6d19bd1fbb0be672f2d5aa
Jose Beltran <jbeltran@doterra.com>
2019-10-23 12:42:28 -0600

    login page

--- web/model/user-model.php
+++ web/model/user-model.php
@@ -1,8 +1,5 @@
 <?php 
 
-    require_once '../library/connection.php';
-    require_once '../library/function.php';
-
     function getClient($username) {
         $db = connect_db();
         $sql = 'SELECT id, username, password, email

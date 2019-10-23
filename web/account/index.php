c72a553125648844a462d5cf40ce74c368270e0d
Jose Beltran <jbeltran@doterra.com>
2019-10-23 13:07:54 -0600

    login page

--- web/account/index.php
+++ web/account/index.php
@@ -35,27 +35,26 @@
             // Compare the password just submitted against
             // the hashed password for the matching client
 
-            // $hashCheck = password_verify($password, $clientData['password']);
+            $hashCheck = password_verify($password, $clientData['password']);
 
             // If the hashes don't match create an error
             // and return to the login view
 
-            // if (!$hashCheck) {
-            //     $message = '<p class="notice">Please check your password and try again.</p>';
-            //     include '../view/login.php';
-            //     exit;
-            // }
+            if (!$hashCheck) {
+                $message = '<p class="notice">Please check your password and try again.</p>';
+                include '../view/login.php';
+                exit;
+            }
 
             // A valid user exists, log them in
             $_SESSION['loggedin'] = TRUE;
-            // Remove the password from the array
-            // the array_pop function removes the last
-            // element from an array
-            array_pop($clientData);
+
             // Store the array into the session
             $_SESSION['clientData'] = $clientData;
+
             setcookie('firstname', '', strtotime('-1 year'), '/');
-            // Send them to the admin view
+
+            // Send them to the account view
             include '../view/account.php';
             exit;
             break;

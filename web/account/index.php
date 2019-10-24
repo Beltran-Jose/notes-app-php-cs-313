71cd743b0a7480362966f24228a5db92facb16f4
Jose Beltran <jbeltran@doterra.com>
2019-10-24 11:14:00 -0600

    registration page

--- web/account/index.php
+++ web/account/index.php
@@ -1,58 +1,100 @@
 <?php
+require_once '../library/connection.php';
+require_once '../library/function.php';
+require_once '../model/user-model.php';
 
-    require_once '../library/connection.php';
-    require_once '../library/function.php';
-    require_once '../model/user-model.php';
+// Create or access a Session
+session_start();
 
-    // Create or access a Session
-    session_start();
+//variables
 
-    //variables
-
-    $action = filter_input(INPUT_POST, 'action');
-    if ($action == NULL) {
-        $action = filter_input(INPUT_GET, 'action');
-    }
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+}
 
 
-    switch ($action) {
-        case 'login':
-            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_EMAIL);
-            $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-            // $passwordCheck = checkPassword($password);
+switch ($action) {
+    case 'login':
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+        // $passwordCheck = checkPassword($password);
 
-            // Run basic checks, return if errors
-            if (empty($username) || empty($password)) {
-                $message = '<p class="notice">Please provide a valid email address and password.</p>';
-                include '../index.php';
-                exit;
-            }
-
-            // A valid password exists, proceed with the login process
-            $clientData = getClient($username);
-
-            if ($password != $clientData['password']) {
-                $message = '<p class="notice">Please check your password and try again.</p>';
-                include '../home.php';
-                exit;
-            }
-
-            // A valid user exists, log them in
-            $_SESSION['loggedin'] = TRUE;
-
-            // Store the array into the session
-            $_SESSION['clientData'] = $clientData;
-
-            setcookie('firstname', '', strtotime('-1 year'), '/');
-
-            // Send them to the account view
-            include '../view/account.php';
+        // Run basic checks, return if errors
+        if (empty($username) || empty($password)) {
+            $message = '<p class="notice">Please provide a valid email address and password.</p>';
+            include '../index.php';
             exit;
-            break;
+        }
 
-        default:
-            include 'home.php';
+        // A valid password exists, proceed with the login process
+        $clientData = getClient($username);
 
-    }
+        if ($password != $clientData['password']) {
+            $message = '<p class="notice">Please check your password and try again.</p>';
+            include '../home.php';
+            exit;
+        }
+
+        // A valid user exists, log them in
+        $_SESSION['loggedin'] = TRUE;
+
+        // Store the array into the session
+        $_SESSION['clientData'] = $clientData;
+
+        setcookie('firstname', '', strtotime('-1 year'), '/');
+
+        // Send them to the account view
+        include '../view/account.php';
+        exit;
+        break;
+
+
+    case 'register':
+        // Filter and store the data
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+        $email = checkEmail($email);
+        $existingEmail = checkExistingEmail($email);
+
+        // Check for existing email address in the table
+        if ($existingEmail) {
+            $message = '<p class="notice">That email address already exists. Do you want to login instead?</p>';
+            include '../view/login.php';
+            exit;
+        }
+
+        // Check for missing data
+        if (empty($username) || empty($password) || empty($email)) {
+            $message = '<p>Please provide information for all empty form fields.</p>';
+            include '../view/registration.php';
+            exit;
+        }
+
+
+        // Send the data to the model
+        $regOutcome = regClient($username, $password, $email);
+
+        // Check and report the result
+        if ($regOutcome === 1) {
+            setcookie('firstname', $username, strtotime('+1 year'), '/');
+            // $message = "<p>Thanks for registering $username. Please use your email and password to login.</p>";
+            $_SESSION['message'] = "Thanks for registering $username. Please use your email and password to login.";
+            header('Location: /acme/accounts/?action=login');
+            // include '../view/login.php';
+            exit;
+        } else {
+            $message = "<p>Sorry $username, but the registration failed. Please try again.</p>";
+            include '../view/registration.php';
+            exit;
+        }
+        break;
+
+    default:
+        include 'home.php';
+
+}
 
 ?>
--- web/model/user-model.php
+++ web/model/user-model.php
@@ -4,7 +4,7 @@
         $db = connect_db();
         $sql = 'SELECT id, username, password, email
         FROM users
-        WHERE username =:username';
+        WHERE username =: username ';
         $stmt = $db -> prepare($sql);
         $stmt -> bindValue(':username', $username, PDO::PARAM_STR);
         $stmt -> execute();
@@ -13,22 +13,21 @@
         return $clientData;
     }
 
-    function regClient($clientFirstname, $clientLastname, $username, $clientPassword) {
+    function regClient($username, $password, $email) {
         // Create a connection object using the acme connection function
-        $db = acmeConnect();
+        $db = connect_db();
         // The SQL statement
-        $sql = 'INSERT INTO clients (clientFirstname, clientLastname,username, clientPassword)
-        VALUES(: clientFirstname,: clientLastname,: username,: clientPassword)
+        $sql = 'INSERT INTO users (username, password, email)
+        VALUES(: username,: password,: email)
         ';
         // Create the prepared statement using the acme connection
         $stmt = $db -> prepare($sql);
         // The next four lines replace the placeholders in the SQL
         // statement with the actual values in the variables
         // and tells the database the type of data it is
-        $stmt -> bindValue(':clientFirstname', $clientFirstname, PDO::PARAM_STR);
-        $stmt -> bindValue(':clientLastname', $clientLastname, PDO::PARAM_STR);
         $stmt -> bindValue(':username', $username, PDO::PARAM_STR);
-        $stmt -> bindValue(':clientPassword', $clientPassword, PDO::PARAM_STR);
+        $stmt -> bindValue(':password', $password, PDO::PARAM_STR);
+        $stmt -> bindValue(':email', $email, PDO::PARAM_STR);
         // Insert the data
         $stmt -> execute();
         // Ask how many rows changed as a result of our insert
@@ -42,8 +41,8 @@
 
     // Check for an existing username address
     function checkExistingusername($username) {
-        $db = acmeConnect();
-        $sql = 'SELECT username FROM clients WHERE username = :username';
+        $db = connect_db();
+        $sql = 'SELECT username FROM users WHERE username = :username';
         $stmt = $db -> prepare($sql);
         $stmt -> bindValue(':username', $username, PDO::PARAM_STR);
         $stmt -> execute();
@@ -56,23 +55,21 @@
         }
     }
 
-
-    function checkForDuplicateUpdateusername($username, $clientId) {
-        $db = acmeConnect();
-        $sql = 'SELECT username FROM clients WHERE username = :username AND clientId != :id';
+    // Check for an existing email address
+    function checkExistingEmail($clientEmail) {
+        $db = connect_db();
+        $sql = 'SELECT email FROM users WHERE email = :email';
         $stmt = $db -> prepare($sql);
-        $stmt -> bindValue(':username', $username, PDO::PARAM_STR);
-        $stmt -> bindValue(':id', $clientId, PDO::PARAM_INT);
+        $stmt -> bindValue(':email', $email, PDO::PARAM_STR);
         $stmt -> execute();
-        $matchusername = $stmt -> fetch(PDO::FETCH_NUM);
+        $matchEmail = $stmt -> fetch(PDO::FETCH_NUM);
         $stmt -> closeCursor();
-        if (empty($matchusername)) {
+        if (empty($matchEmail)) {
             return 0;
         } else {
             return 1;
         }
     }
-
 
 ?>
 
